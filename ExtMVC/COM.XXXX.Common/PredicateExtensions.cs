34e473689f14a49b49cb6a08a989552460ef8619
Eric <ailaohuyou123@qq.com>
2015-06-15 16:31:32 +0800

    extend basecontroller

---
+++ ExtMVC/COM.XXXX.Common/PredicateExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace COM.XXXX.Common
+{ 
+    /// <summary>  
+    /// 构造函数使用True时：单个AND有效，多个AND有效；单个OR无效，多个OR无效；混合时写在AND后的OR有效  
+    /// 构造函数使用False时：单个AND无效，多个AND无效；单个OR有效，多个OR有效；混合时写在OR后面的AND有效  
+    /// </summary>  
+
+    public static class PredicateExtensions
+    {
+        //public static Expression<Func<T, bool>> True<T>() { return f => true; }
+
+        //public static Expression<Func<T, bool>> False<T>() { return f => false; }
+
+        //public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> expression1,
+
+        //Expression<Func<T, bool>> expression2)
+        //{
+        //    var invokedExpression = Expression.Invoke(expression2,expression1.Parameters.Cast<Expression>());
+        //    return Expression.Lambda<Func<T, bool>>(Expression.Or(expression1.Body, invokedExpression),expression1.Parameters);
+        //}
+
+        //public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expression1,Expression<Func<T, bool>> expression2)
+        //{
+        //    var invokedExpression = Expression.Invoke(expression2,expression1.Parameters.Cast<Expression>());
+        //    return Expression.Lambda<Func<T, bool>>(Expression.And(expression1.Body,invokedExpression), expression1.Parameters);
+        //}
+        public static Expression<Func<T, bool>> True<T>() { return f => true; }
+        public static Expression<Func<T, bool>> False<T>() { return f => false; }
+        public static Expression<T> Compose<T>(this Expression<T> first, Expression<T> second, Func<Expression, Expression, Expression> merge)
+        {
+            // build parameter map (from parameters of second to parameters of first)  
+            var map = first.Parameters.Select((f, i) => new { f, s = second.Parameters[i] }).ToDictionary(p => p.s, p => p.f);
+            // replace parameters in the second lambda expression with parameters from the first  
+            var secondBody = ParameterRebinder.ReplaceParameters(map, second.Body);
+            // apply composition of lambda expression bodies to parameters from the first expression   
+            return Expression.Lambda<T>(merge(first.Body, secondBody), first.Parameters);
+        }
+
+        public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> first, Expression<Func<T, bool>> second)
+        {
+            return first.Compose(second, Expression.And);
+        }
+
+        public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> first, Expression<Func<T, bool>> second)
+        {
+            return first.Compose(second, Expression.Or);
+        }
+
+    }
+
+
+}
--- ExtMVC/COM.XXXX.Controllers/ControllerBase.cs
+++ ExtMVC/COM.XXXX.Controllers/ControllerBase.cs
@@ -14,7 +14,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 using Com.XXXX.Class;
 using Com.XXXX.Common;
 using COM.XXXX.Models.Admin;
@@ -23,6 +26,7 @@ using Repository.Domain.Infrastructure;
 
 namespace COM.XXXX.Controllers
 {
+   
     public class ControllerBase:Controller
     {
         private IUnitOfWork UnitOfWork { get; set; }
@@ -53,10 +57,40 @@ namespace COM.XXXX.Controllers
         {
             if (null == CurrentUser)
             {
-                filterContext.Result = RedirectToAction("Index", "Account");
+                filterContext.Result = Redirect("~/Account/Index");
             }
         }
 
+        /// <summary>
+        /// 返回JsonResult.24         /// </summary>
+        /// <param name="data">数据</param>
+        /// <param name="behavior">行为</param>
+        /// <param name="format">json中dateTime类型的格式</param>
+        /// <returns>Json</returns>
+        protected JsonResult MyJson(object data, JsonRequestBehavior behavior, string format)
+        {
+            return new CustomJsonResult
+            {
+                Data = data,
+                JsonRequestBehavior = behavior,
+                FormateStr = format
+            };
+        }
+
+        /// <summary>
+        /// 返回JsonResult42         /// </summary>
+        /// <param name="data">数据</param>
+        /// <param name="format">数据格式</param>
+        /// <returns>Json</returns>
+        protected JsonResult MyJson(object data, string format)
+        {
+            return new CustomJsonResult
+            {
+                Data = data,
+                JsonRequestBehavior = JsonRequestBehavior.DenyGet,
+                FormateStr = format
+            };
+        }
         /// <summary>
         /// 当前用户信息
         /// </summary>
@@ -75,4 +109,74 @@ namespace COM.XXXX.Controllers
             }
         }
     }
+    /// <summary>
+    /// 自定义Json视图
+    /// </summary>
+    public class CustomJsonResult : JsonResult
+    {
+        /// <summary>
+        /// 格式化字符串
+        /// </summary>
+        public string FormateStr
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 重写执行视图
+        /// </summary>
+        /// <param name="context">上下文</param>
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            HttpResponseBase response = context.HttpContext.Response;
+
+            if (string.IsNullOrEmpty(this.ContentType))
+            {
+                response.ContentType = this.ContentType;
+            }
+            else
+            {
+                response.ContentType = "application/json";
+            }
+
+            if (this.ContentEncoding != null)
+            {
+                response.ContentEncoding = this.ContentEncoding;
+            }
+
+            if (this.Data != null)
+            {
+                JavaScriptSerializer jss = new JavaScriptSerializer();
+                string jsonString = jss.Serialize(Data);
+                string p = @"\\/Date\((\d+)\)\\/";
+                MatchEvaluator matchEvaluator = new MatchEvaluator(this.ConvertJsonDateToDateString);
+                Regex reg = new Regex(p);
+                jsonString = reg.Replace(jsonString, matchEvaluator);
+
+                response.Write(jsonString);
+            }
+        }
+
+        /// <summary>  
+        /// 将Json序列化的时间由/Date(1294499956278)转为字符串 .
+        /// </summary>  
+        /// <param name="m">正则匹配</param>
+        /// <returns>格式化后的字符串</returns>
+        private string ConvertJsonDateToDateString(Match m)
+        {
+            string result = string.Empty;
+            DateTime dt = new DateTime(1970, 1, 1);
+            dt = dt.AddMilliseconds(long.Parse(m.Groups[1].Value));
+            dt = dt.ToLocalTime();
+            result = dt.ToString(FormateStr);
+            return result;
+        }
+    }
+
 }
