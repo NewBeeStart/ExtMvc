cb7350df27e16516acc6c4fd15d8eb6cd077a03e
Eric.Tsui <190241347@qq.com>
2015-06-16 23:44:46 +0800

    add WebsiteChannel Manager,but should modify and codeSmith Template of List of Page

---
+++ ExtMVC/Repository.DAL/Repository/Cms/CmsChannelRepository.cs
@@ -0,0 +1,25 @@
+﻿/*----------------------------------------------------------------
+// Copyright (C) 2014 NewBee工作室
+// 版权所有。 
+//
+// 文件名：ButtonRepository
+// 文件功能描述：
+//
+// 创建标识：xycui 2014/12/15 10:23:56
+//
+// 修改标识：
+// 修改描述：
+//----------------------------------------------------------------*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using COM.XXXX.Models.CMS;
+using Repository.Domain.Infrastructure;
+
+namespace Repository.DAL.Repository
+{ 
+    public class CmsChannelRepository : Repository<Cms_Channel>
+    {
+    }
+}
